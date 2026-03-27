62bee2f7ac46b06c37ca6e5e52a98140a3215120
bongiweqanda87 <bongiweqanda87@gmail.com>
2026-03-27 11:23:25 +0200

    Dice Rolling Simulator

--- src/projects/project09_Dice_Roller/Main.java
+++ src/projects/project09_Dice_Roller/Main.java
@@ -38,31 +38,66 @@ public class Main {
 
     static void printDie(int roll){
         String dice1 = """
-                -------
-               |        |
-               |   ●    |
-               |        |
-                -------
+                -----
+                |   |
+                | o |
+                |   |
+                -----
+                
                 """;
         String dice2 = """
-                -------
-               |  ●     |
-               |        |
-               |     ●  |
-                -------
+                
+                -----
+                |o  |
+                |   |
+                |  o|
+                -----
                 """;
 
         String dice3 = """
-                -------
-               | ●     |
-               |   ●   |
-               |      ●|
-                -------
+                -----
+                |o  |
+                | o |
+                |  o|
+                -----
+                
                 """;
 
+        String dice4 =
+                """
+                        -----
+                        |o o|
+                        |   |
+                        |o o|
+                        -----
+                        
+                        """;
+        String dice5 =
+                """
+                        -----
+                        |o o|
+                        | o |
+                        |o o|
+                        -----
+                        """;
 
+        String dice6 =
+                """
+                        -----
+                        |o o|
+                        |o o|
+                        |o o|
+                        -----
+                        """;
+        switch (roll){
+            case 1 -> System.out.print(dice1);
+            case 2 -> System.out.print(dice2);
+            case 3 -> System.out.print(dice3);
+            case 4 -> System.out.print(dice4);
+            case 5 -> System.out.print(dice5);
+            case 6 -> System.out.print(dice6);
+            default -> System.out.println("Invalid number to roll");
 
-        System.out.println(dice1);
-
+        }
     }
 }
