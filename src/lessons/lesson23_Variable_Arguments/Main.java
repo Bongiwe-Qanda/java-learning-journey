6d5a733c8444389dab3be497d3e420e9b801012d
bongiweqanda87 <bongiweqanda87@gmail.com>
2026-03-28 22:06:52 +0200

    Variables args lesson

---
+++ src/lessons/lesson23_Variable_Arguments/Main.java
@@ -0,0 +1,15 @@
+package lessons.lesson23_Variable_Arguments;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        varargs -> allows a method to accept a varying number og arguments.
+                -> makes methods more flexible(no overload methods are needed)
+                -> java packs arguments into an array
+                -> ...j
+
+
+
+         */
+    }
+}
