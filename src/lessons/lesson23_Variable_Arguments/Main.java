d43c86c005aa8deaf8c19b0e9c702d5b1bba8a07
bongiweqanda87 <bongiweqanda87@gmail.com>
2026-03-30 11:05:12 +0200

    Variable Arguments Lesson

--- src/lessons/lesson23_Variable_Arguments/Main.java
+++ src/lessons/lesson23_Variable_Arguments/Main.java
@@ -3,14 +3,43 @@ package lessons.lesson23_Variable_Arguments;
 public class Main {
     public static void main(String[] args) {
         /*
-        varargs -> allows a method to accept a varying number og arguments.
+        varargs -> allows a method to accept a varying number of arguments.
                 -> makes methods more flexible(no overload methods are needed)
-                -> java packs arguments into an array
+                -> java compiler packs arguments into an array
+                -> one method that accepts many different variables
                 -> ...j
          */
-    }
-}
 
+        System.out.println(add(1,2,3,4));
+        System.out.println(average(2,3,4,5));
+        }
+
+        static int add(int... numbers){
+
+        int sum = 0;
+
+
+        for(int number : numbers){
+            sum += number;
+        }
+        return sum;
+        }
+        static double average(int... numbers){
+
+        double sum = 0;
+
+        if(numbers.length == 0){
+            return 0;
+        }
+        else{
+        for(double number: numbers){
+            sum += number;
+        }
+        return sum /  numbers.length;
+        }
+
+}
+}
 
 
 
