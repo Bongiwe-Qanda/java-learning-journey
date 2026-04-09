58753131f55fc961731e8c0b8ae11ba4c9e3aa15
boqancpt025 <boqancpt025@student.wethinkcode.co.za>
2026-04-09 12:17:26 +0200

    Rock Paper Scissors

--- src/projects/project11_Rock_Paper_Scissors/Main.java
+++ src/projects/project11_Rock_Paper_Scissors/Main.java
@@ -12,47 +12,53 @@ public class Main {
         3. Re-prompt user
         4.
          */
-
-        String user_choice;
-        String computer_choice;
-        String[] computer_choices = {"rock","paper","scissors"};
-        String playAgain = "yes";
-
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
 
-        System.out.print("Enter your choice (rock,paper,scissors): ");
-        user_choice = scanner.nextLine().toLowerCase();
+        String user_choice;
+        String computer_choice;
+        String[] computer_choices = {"rock","paper","scissors"};
+        String want_to_continue = "yes";
 
-        if(!user_choice.equals("rock") &&
-           !user_choice.equals("paper") &&
-                !user_choice.equals("scissors")){
-            System.out.println("Invalid choice");
-        }
+        do{
+            System.out.print("Enter your choice (rock,paper,scissors): ");
+            user_choice = scanner.nextLine().toLowerCase();
+
+            if(!user_choice.equals("rock") &&
+                    !user_choice.equals("paper") &&
+                    !user_choice.equals("scissors")){
+                System.out.println("Invalid choice");
+            }
 
 
-        computer_choice = computer_choices[random.nextInt(3)];
-        System.out.printf("Computer choice: %s\n", computer_choice);
+            computer_choice = computer_choices[random.nextInt(3)];
+            System.out.printf("Computer choice: %s\n", computer_choice);
 
-        if(user_choice.equals(computer_choice)){
-            System.out.println("It's a tie!!");
-        }
-        else if(user_choice.equals("rock") && computer_choice.equals("scissors")){
-            System.out.println("You win!!!🌟");
-        }
-        else if(user_choice.equals("paper") && computer_choice.equals("rock")){
-            System.out.println("You win!!!🌟");
-        }
-        else if(user_choice.equals("scissors") && computer_choice.equals("paper")){
-            System.out.println("You win!!!🌟");
+            if(user_choice.equals(computer_choice)){
+                System.out.println("It's a tie!!");
+            }
+            else if(user_choice.equals("rock") && computer_choice.equals("scissors")){
+                System.out.println("You win!!!🌟");
+            }
+            else if(user_choice.equals("paper") && computer_choice.equals("rock")){
+                System.out.println("You win!!!🌟");
+            }
+            else if(user_choice.equals("scissors") && computer_choice.equals("paper")){
+                System.out.println("You win!!!🌟");
 
-        }
-        else{
-            System.out.println("You lose!!!👎");
-        }
-        System.out.print("Do you want to continue playing?: ");
-        String want_to_continue = scanner.nextLine();
+            }
+            else{
+                System.out.println("You lose!!!👎");
+            }
+
+            System.out.print("Do you want to continue playing? (yes/no): ");
+            want_to_continue = scanner.nextLine().toLowerCase();
+
+
+        }while(want_to_continue.equals("yes"));
+
+        System.out.println("Thank you for playing!!");
 
 
     scanner.close();
