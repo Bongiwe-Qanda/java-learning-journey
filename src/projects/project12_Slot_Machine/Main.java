d6b2d9f46ba2033b8adea3b4f36ea321d295a033
Bongiwe Qanda <bongiweqanda87@gmail.com>
2026-04-10 11:35:13 +0200

    Slot Machine

--- src/projects/project12_Slot_Machine/Main.java
+++ src/projects/project12_Slot_Machine/Main.java
@@ -1,4 +1,39 @@
 package projects.project12_Slot_Machine;
+import java.util.Scanner;
 
 public class Main {
+    public Main() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        // JAVA SLOT MACHINE
+
+        // Declare variables
+        Scanner scanner = new Scanner(System.in);
+        int balance = 50;
+        int bet;
+        int payout;
+        String[] row;
+
+        // Display welcome message
+
+        System.out.println("WELCOME TO JAVA SLOTS");
+        System.out.println("Symbols : ");
+        // Play if Balance > 0
+        // Enter bet amount
+            // Verify if bet > balance
+            //  Verify if bet > 0
+            // Subtract bet from balance
+
+        // Spin row
+        // Print row
+        // Get a payout if there are matches
+        // Ask to play again
+        // Display exit message
+
+    scanner.close();
+    }
+
+
 }
