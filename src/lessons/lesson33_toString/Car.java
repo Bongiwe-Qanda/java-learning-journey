4e4ec3ea413a69d6229cda18444704b7b32b6462
bongiweqanda87 <bongiweqanda87@gmail.com>
2026-05-01 13:35:10 +0200

    .toString method Lesson

---
+++ src/lessons/lesson33_toString/Car.java
@@ -0,0 +1,24 @@
+package lessons.lesson33_toString;
+
+public class Car {
+    String make ;
+    String model;
+    int year;
+    String colour;
+
+    Car(String make, String model, int year, String colour) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString(){
+        return this.colour + " " + this.year +" "+ this.make +" "+ this.model;
+
+    }
+
+
+}
+
